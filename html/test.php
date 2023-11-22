c73f9233ce3e6bc3207edf264e160fa148e06b07
bowtsogakow <deleonzymon@gmail.com>
2023-11-22 21:02:24 +0800

    DB functions

--- html/test.php
+++ html/test.php
@@ -8,8 +8,25 @@
 <body>
     <?php
         include "DBQuery.php"; 
-        $id = LogIn("1","1","1"); 
-        echo "$id";
+        $cartID = []; 
+        $Info = []; 
+        $cartID = getList_cart(11); 
+        $i; 
+
+        getProductInfo(11);
+        foreach($cartID as $i){
+            $productID = getProduct($i);
+            $Info = getProductInfo($productID);
+            $cart = checkExistingCart(1, $productID);
+            echo $i . " " . $productID . " " . $Info[0] . $Info[1] . " " . $cart . " " . "<br>"; 
+        }
+
+        $add = addtoCart(11, 20);
+        $remove = removeFromCart(8); 
+        $latest = getLatestOrder(14); 
+
+        echo "<br>" . $add . " " . $remove . " " . $latest; 
+        
     ?>
 </body>
 </html>
